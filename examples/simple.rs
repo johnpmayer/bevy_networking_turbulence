cc2fbe742cf1d1fe923677fe27eb4021e43e202a
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2021-02-06 21:38:53 -0500

    including the code changes

--- examples/simple.rs
+++ examples/simple.rs
@@ -12,6 +12,8 @@ mod utils;
 use utils::*;
 
 const SERVER_PORT: u16 = 14191;
+const DESYNC_THRESH: f64 = 5.;
+const PING_FREQ: f64 = 1.;
 
 fn main() {
     cfg_if::cfg_if! {
@@ -44,17 +46,19 @@ fn main() {
 }
 
 fn startup(mut net: ResMut<NetworkResource>, args: Res<Args>) {
-    cfg_if::cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            // FIXME: set this address to your local machine
-            let mut socket_address: SocketAddr = "192.168.1.20:0".parse().unwrap();
-            socket_address.set_port(SERVER_PORT);
-        } else {
-            let ip_address =
-                bevy_networking_turbulence::find_my_ip_address().expect("can't find ip address");
-            let socket_address = SocketAddr::new(ip_address, SERVER_PORT);
-        }
-    }
+    // cfg_if::cfg_if! {
+        let mut socket_address: SocketAddr = "127.0.0.1:0".parse().unwrap();
+        socket_address.set_port(SERVER_PORT);
+        // if #[cfg(target_arch = "wasm32")] {
+        //     // FIXME: set this address to your local machine
+        //     let mut socket_address: SocketAddr = "127.0.0.1:0".parse().unwrap();
+        //     socket_address.set_port(SERVER_PORT);
+        // } else {
+        //     let ip_address =
+        //         bevy_networking_turbulence::find_my_ip_address().expect("can't find ip address");
+        //     let socket_address = SocketAddr::new(ip_address, SERVER_PORT);
+        // }
+    // }
 
     #[cfg(not(target_arch = "wasm32"))]
     if args.is_server {
@@ -67,17 +71,25 @@ fn startup(mut net: ResMut<NetworkResource>, args: Res<Args>) {
     }
 }
 
-fn send_packets(mut net: ResMut<NetworkResource>, time: Res<Time>, args: Res<Args>) {
-    if !args.is_server {
-        if (time.seconds_since_startup() * 60.) as i64 % 60 == 0 {
+fn send_packets(mut net: ResMut<NetworkResource>, mut state: ResMut<NetworkReader>, time: Res<Time>, args: Res<Args>) {
+    if !args.is_server && state.connected {
+        let now = time.seconds_since_startup();
+        if now > state.last_ping + PING_FREQ {
             log::info!("PING");
             net.broadcast(Packet::from("PING"));
+            state.last_ping = now;
         }
+    }
+    if state.last_ping > state.last_pong + DESYNC_THRESH {
+        log::warn!("Probably disconnected...")
     }
 }
 
 #[derive(Default)]
 struct NetworkReader {
+    connected: bool,
+    last_ping: f64,
+    last_pong: f64,
     network_events: EventReader<NetworkEvent>,
 }
 
@@ -86,14 +98,26 @@ fn handle_packets(
     time: Res<Time>,
     mut state: ResMut<NetworkReader>,
     network_events: Res<Events<NetworkEvent>>,
+    args: Res<Args>,
 ) {
+    let now = time.seconds_since_startup();
     for event in state.network_events.iter(&network_events) {
         match event {
+            NetworkEvent::Connected(_handle) => {
+                log::info!("Connected");
+                state.connected = true;
+            }
+            NetworkEvent::Disconnected(_handle) => {
+                log::info!("Disconnected");
+                state.connected = false;
+            }
             NetworkEvent::Packet(handle, packet) => {
                 let message = String::from_utf8_lossy(packet);
                 log::info!("Got packet on [{}]: {}", handle, message);
-                if message == "PING" {
-                    let message = format!("PONG @ {}", time.seconds_since_startup());
+                if !args.is_server && message.starts_with("PONG") {
+                    state.last_pong = now;
+                } else if message == "PING" {
+                    let message = format!("PONG @ {}", now);
                     match net.send(*handle, Packet::from(message)) {
                         Ok(()) => {
                             log::info!("Sent PONG");
@@ -104,7 +128,6 @@ fn handle_packets(
                     }
                 }
             }
-            _ => {}
         }
     }
 }
